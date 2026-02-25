2a06868e98a6b094963c92d9c8e78fbd368fbe48
AngeloParrinello <61511110+AngeloParrinello@users.noreply.github.com>
2026-02-25 10:03:47 +0100

    feat: more chap 6 stuff

--- example-chapter-6/chapter-6.go
+++ example-chapter-6/chapter-6.go
@@ -6,6 +6,15 @@ func stringp(s string) *string {
 	return &s
 }
 
+func failedUpdate(g *int) {
+	x := 10
+	g = &x // this does not update the value of g in the caller, it only updates the local copy of g in this function
+}
+
+func update(g *int) {
+	*g = 20 // this updates the value at the memory address that g points to, which is the same memory address that the caller's pointer points to
+}
+
 func main() {
 	x := "hello"
 	pointerToX := &x // & is the address operator, it gives us the memory address of x
@@ -39,4 +48,18 @@ func main() {
 	//   and returns a pointer to that value
 	p := person{name: "Alice", surname: stringp("Smith"), age: 30}
 	fmt.Println(p)
+
+	// Rather than declare taht some variables and parameters are immutable, Go developers use pointers to indicate that a parameter is mutable.
+	// if a pointer is passed to a function, the fucntion gets a copy of the pointer.
+	// Two implications:
+	// - when you pass a nil pointer to a function you cannot make the value non nil
+	var f *int // f is nil
+	failedUpdate(f)
+	fmt.Println("f after failedUpdate:", f) // f is still nil
+	// -if you want the value assigned to a pointer parameter to still bne there when you exit the function, you must dereference the pointer and set the value.
+	a := 10
+	failedUpdate(&a)
+	fmt.Println("a after failedUpdate:", a) // a is still 10
+	update(&a)
+	fmt.Println("a after update:", a) // a is now 20
 }
